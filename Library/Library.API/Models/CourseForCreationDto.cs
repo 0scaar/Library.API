8ee834dee87ec700b5e36dcc3aaf43cdc0fd628d
Oscar <oscaar_2003@hotmail.com>
2020-02-08 17:56:34 -0300

    customizing error message

--- Library/Library.API/Models/CourseForCreationDto.cs
+++ Library/Library.API/Models/CourseForCreationDto.cs
@@ -4,23 +4,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Library.API.Models
 {
-    [CourseTitleMustBeDifferentFromDescriptionAttribute]
-    public class CourseForCreationDto //: IValidatableObject
+    [CourseTitleMustBeDifferentFromDescriptionAttribute(
+        ErrorMessage = "Title must be different from description")]
+    public class CourseForCreationDto 
     {
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
         public string Title { get; set; }
 
-        [MaxLength(1500)]
+        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
         public string Description { get; set; }
-
-        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        //{
-        //    if (Title == Description)
-        //    {
-        //        yield return new ValidationResult("The provided description should be different from the title.",
-        //            new[] { "CourseForCreationDto" });
-        //    }
-        //}
     }
 }

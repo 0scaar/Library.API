3d017e74a3efae1287e0787a87d53576cde7a85f
Oscar <oscaar_2003@hotmail.com>
2020-02-10 20:13:39 -0300

    configurating the status code 422 for validation errors in the startup.cs

--- Library/Library.API/Startup.cs
+++ Library/Library.API/Startup.cs
@@ -4,6 +4,7 @@ using Library.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,7 +31,29 @@ namespace Library.API
                 setupAction.ReturnHttpNotAcceptable = true;
             })
             // Accept : application/xml (Header)
-            .AddXmlDataContractSerializerFormatters();
+            .AddXmlDataContractSerializerFormatters()
+            // Configurating the status code 422 for validation errors
+            .ConfigureApiBehaviorOptions(setupAction => 
+            {
+                setupAction.InvalidModelStateResponseFactory = context =>
+                {
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Type = "https://library.com/modelvalidationproblem",
+                        Title = "One or more model validation errors ocurred.",
+                        Status = StatusCodes.Status422UnprocessableEntity,
+                        Detail = "See the errors property for details.",
+                        Instance = context.HttpContext.Request.Path
+                    };
+
+                    problemDetails.Extensions.Add("traceId", context.HttpContext.TraceIdentifier);
+
+                    return new UnprocessableEntityObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json" }
+                    };
+                };
+            });
 
             // Configuration of AutoMapper
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
